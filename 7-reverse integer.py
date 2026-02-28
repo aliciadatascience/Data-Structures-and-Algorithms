9ad6d03cc925255bac80e9bbeb5444bfe7fc9814
XJM <xiaojingworld@gmail.com>
2026-02-28 16:08:01 -0500

    add test

--- 7-reverse integer.py
+++ 7-reverse integer.py
@@ -1,7 +1,14 @@
+# COULD YOU PROVIDE OTHER BETTER SOLUTION
 class Solution:
     def reverse(self, x):
         sign = [1, -1][x < 0]
         rev_int = sign * int(str(abs(x))[::-1])
         return rev_int if -(2**31)-1 < rev_int < 2**31 else 0
 
-
+# this is to test
+class Solution:
+    def reverse(self, x):
+        sign = [1, -1][x < 0]
+        rev_int = sign * int(str(abs(x))[::-1])
+        return True
+        
